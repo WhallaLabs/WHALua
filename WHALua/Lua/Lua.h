3cfb539bd7a74152cd9583650d2770a17be6487a
Szymon Kuczur <szymon.kuczur@whallalabs.com>
2015-07-23 10:47:38 +0200

    Lua lib

---
+++ WHALua/Lua/Lua.h
@@ -0,0 +1,13 @@
+//
+//  Lua.h
+//  Lua
+//
+//  Created by Szymon Kuczur on 22.07.2015.
+//  Copyright (c) 2015 Whallalabs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Lua : NSObject
+
+@end
